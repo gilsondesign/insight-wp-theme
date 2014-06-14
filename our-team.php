5432e19dce41fa9a1ce0141375658c74d4b5f0d7
Adam <Gilson>
2014-06-13 22:31:25 -0500

    adding our team styling

--- our-team.php
+++ our-team.php
@@ -7,7 +7,29 @@ Template Name: Our Team
 
 <?php get_header(); ?>   
     
-    
+<div id="our-team">
+    <section id="main-wrapper">
+        <div class="container" >
+            <h2>Our team</h2>
+            <p>Since 1996, Peters & Associates has provided forensic and investigative accounting services to over 150 insurance companies and law firms. Each partner brings more than 20 years of forensic accounting experience, resulting in unmatched knowledge, expertise and first-hand experience.</p>
+            
+            <ul id="team-bios">
+                <li>
+                    <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                    <h3>Curt Reynolds</h3>
+                    <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            
+        </div>
+    </div>
+</div>    
     
 
 
