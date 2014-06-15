703e61fe61b752028973b9e498bad3b25774197d
Adam Gilson <adam@gilsondesign.com>
2014-06-15 12:21:31 -0500

    start of button styling

--- our-team.php
+++ our-team.php
@@ -18,6 +18,7 @@ Template Name: Our Team
                     <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
                     <h3>Curt Reynolds</h3>
                     <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                    <a href="mailto:curt@insightforensics.com" class="btn">email Curt</a>
                 </li>
                 <li></li>
                 <li></li>
