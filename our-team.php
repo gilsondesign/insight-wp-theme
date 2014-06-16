bd1aff51ee312a9fa4057ac9c6816a351b98447e
Adam Gilson <adam@gilsondesign.com>
2014-06-15 20:32:53 -0500

    our team page styling

--- our-team.php
+++ our-team.php
@@ -11,22 +11,147 @@ Template Name: Our Team
     <section id="main-wrapper">
         <div class="container" >
             <h2>Our team</h2>
-            <p>Since 1996, Peters & Associates has provided forensic and investigative accounting services to over 150 insurance companies and law firms. Each partner brings more than 20 years of forensic accounting experience, resulting in unmatched knowledge, expertise and first-hand experience.</p>
+            <p class="subhead">Since 1996, Peters & Associates has provided forensic and investigative accounting services to over 150 insurance companies and law firms. Each partner brings more than 20 years of forensic accounting experience, resulting in unmatched knowledge, expertise and first-hand experience.</p>
             
-            <ul id="team-bios">
-                <li>
-                    <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
-                    <h3>Curt Reynolds</h3>
-                    <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
-                    <a href="mailto:curt@insightforensics.com" class="btn">email Curt</a>
-                </li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+            <aside class="staff">
+                <h3>Partners</h3>
+                <ul>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                </ul>
+                <h3>Staff</h3>
+                <ul>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>
+                    <li>
+                        <a href="#knight">Terrence S. Knight</a>
+                        <p>Chicago</p>
+                    </li>               
+                </ul>
+            </aside>
+            
+            <article>
+                <ul id="team-bios">
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                   <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_url'); ?>/img/staff-photos/reynolds.jpg" alt="<?php bloginfo('name'); ?>" />
+                        <h3>Curt Reynolds</h3>
+                        <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
+                        <a href="mailto:curt@insightforensics.com" class="btn">Contact Curt</a>
+                    </li>
+                </ul>
+            </article>
             
         </div>
     </div>
