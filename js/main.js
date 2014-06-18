c459a70ae83f9a53a570e329d87e9f6446ededfc
Daniel Reid <danielc.reid@gmail.com>
2014-06-17 21:02:18 -0500

    fixes #5

--- js/main.js
+++ js/main.js
@@ -3,37 +3,37 @@
 
 $(function () {
     $(".js-toggle-nav").click(function () {
-        $(".js-main-nav").slideToggle("fast")
-    })
+        $(".js-main-nav").slideToggle("fast");
+    });
 });
 
 $(function () {
-    if ($(".js-toggle-nav").css('display') != 'none' ) {
+    if ($(".js-toggle-nav").css('display', '') != 'none' ) {
         $(".js-main-nav li a").click(function () {
-            $(".js-main-nav").slideToggle("fast")
-        })
+            $(".js-main-nav").slideToggle("fast");
+        });
     }
 });
 
 // Not sure if we need this since Resources might be a different page
-$(function () {
-    $("a[href*=#]:not([href=#])").click(function () {
-        var e = this.hash;
-        setTimeout(function () {
-            window.location.hash = e
-        }, 1e3);
-        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-            var t = $(this.hash);
-            t = t.length ? t : $("[name=" + this.hash.slice(1) + "]");
-            if (t.length) {
-                $("html,body").animate({
-                    scrollTop: t.offset().top
-                }, 1e3);
-                return false
-            }
-        }
-    })
-});
+//$(function () {
+//    $("a[href*=#]:not([href=#])").click(function () {
+//        var e = this.hash;
+//        setTimeout(function () {
+//            window.location.hash = e
+//        }, 1e3);
+//        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+//            var t = $(this.hash);
+//            t = t.length ? t : $("[name=" + this.hash.slice(1) + "]");
+//            if (t.length) {
+//                $("html,body").animate({
+//                    scrollTop: t.offset().top
+//                }, 1e3);
+//                return false
+//            }
+//        }
+//    })
+//});
 
 // This should just be for the Homepage
 $('.home .main-nav').headroom({
