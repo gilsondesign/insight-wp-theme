24c501615ef0612b494d5a45f648982bd838c1aa
Adam Gilson <adam@gilsondesign.com>
2014-07-01 09:12:24 -0500

    email address update

--- our-team.php
+++ our-team.php
@@ -85,7 +85,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="bestul">Jennifer Bestul, CPA</h3>
                                 <p>Jennifer is a 1985 graduate of Luther College, Decorah, IA, with a Bachelor of Arts degree, and a Major in Accounting. Since she began her career in 1985, she has specialized in investigative accounting. She has been a licensed CPA since 1987. Jennifer has worked on numerous assignments including business income, property damage, extra expense, worker’s comp, inventory, employee dishonesty, and measurement of lost wages. Jennifer has been deposed, participated in appraisals, testified in court and has lectured before insurance companies on several topics.</p>
-                                <a href="mailto:jbestul@insightforensics.com" class="btn">Contact Jennifer</a>
+                                <a href="mailto:jennifer@insightforensics.com" class="btn">Contact Jennifer</a>
                             </div>
                         </li>
                         <li>
@@ -93,7 +93,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="friestedt">John D. Friestedt, CPA, CFF</h3>
                                 <p>John is a 1981 Cum Laude graduate of the University of Notre Dame. He also received an MBA in 1984 from the University of Texas at Austin. He is a licensed CPA in the states of Wisconsin and Illinois. Since 1986, John has focused his entire career on forensic accounting, including numerous assignments involving lost business income, third party income losses and employee dishonesty losses. John has been deposed a number of times and has testified in court.</p>
-                                <a href="mailto:jfriestedt@insightforensics.com" class="btn">Contact John</a>
+                                <a href="mailto:john@insightforensics.com" class="btn">Contact John</a>
                             </div>
                         </li>
                         <li>
@@ -101,7 +101,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="denson">Kathy M Denson, CPA, CFF, CFE</h3>
                                 <p>Kathy graduated from Northeastern Illinois University (with high honors) in 1983, with a bachelors degree in Business with an emphasis on accounting. She became a licensed CPA in Illinois in 1986 and in Colorado in 1989. Kathy’s experience includes 3 years performing financial statement audits,  but the majority of her career has been devoted to determining lost income of businesses and individuals, and providing expert testimony on financial damages.</p>
-                                <a href="mailto:kmdenson@insightforensics.com" class="btn">Contact Kathy</a>
+                                <a href="mailto:kathy@insightforensics.com" class="btn">Contact Kathy</a>
                             </div>
                         </li>
                         <li>
@@ -109,7 +109,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="knight">Terrence S. Knight, CPA, CFF</h3>
                                 <p>Terry graduated in 1983 from Marquette University, Milwaukee, WI, with a Bachelor of Science in Accounting.  Terry is a licensed CPA in Illinois and has focused his career exclusively on forensic accounting since 1991. He has been engaged in numerous assignments relating to employee dishonesty losses, business income losses, property damage, financial condition assessments, and disability income losses. Terry has been deposed and has testified in court and has presented seminars to members of the insurance industry and legal profession.</p>
-                                <a href="mailto:tknight@insightforensics.com" class="btn">Contact Terrence</a>
+                                <a href="mailto:terry@insightforensics.com" class="btn">Contact Terrence</a>
                             </div>
                         </li>
 
@@ -124,7 +124,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="conway">Kelly M. Conway, CPA</h3>
                                 <p>Kelly has a BS from Northern Michigan University.  She is a CPA and has been with our firm since 2003.  She had prior work experience as a bookkeeper for a construction firm and with a tax/write-up accounting firm.  At Insight, Kelly specializes in 1st party property, and lost income analysis. </p>
-                                <a href="mailto:kconway@insightforensics.com" class="btn">Contact Kelly</a>
+                                <a href="mailto:kelly@insightforensics.com" class="btn">Contact Kelly</a>
                             </div>
                         </li>
                         <li>
@@ -132,7 +132,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="denton">Lisa A. Denton</h3>
                                 <p>Lisa has two BS degrees from Ferris State University.  She has been with our firm since 1999.  She had prior work experience with a tax/write up accounting firm and as a bookkeeper for a radiographic firm.  At Insight she is our Firm Administrator, handling all of our internal accounting/bookkeeping.  She also works on business income losses and provides research assistance.</p>
-                                <a href="mailto:ldenton@insightforensics.com" class="btn">Contact Lisa</a>
+                                <a href="mailto:lisa@insightforensics.com" class="btn">Contact Lisa</a>
                             </div>
                         </li>
                         <li>
@@ -140,7 +140,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="schultz">Jessica M. Schultz, CPA</h3>
                                 <p>Jessie has a BS from Marquette University.  She is a CPA and has been with our firm since 1997.  She had prior work experience as an administrator in a title company.  At Insight, Jessica works on damage assessment for a wide array of insurance and litigated matters. </p>
-                                <a href="mailto:jschultz@insightforensics.com" class="btn">Contact Jessica</a>
+                                <a href="mailto:jessica@insightforensics.com" class="btn">Contact Jessica</a>
                             </div>
                         </li>
                         <li>
@@ -148,7 +148,7 @@ Template Name: Our Team
                             <div class="bio-main">
                                 <h3 id="derose">Ann F. DeRose, CPA</h3>
                                 <p>Ann has a BBA from the University of Wisconsin-Eau Claire.  She is a CPA and has been with us since 2000.  She had prior work experience as a bank manager and with a tax/write-up accounting firm.  At Insight she specializes in the evaluation of lost income matters arising from personal injury, insured events, and torts.  Ann has provided expert witness testimony on behalf of clients.</p>
-                                <a href="mailto:aderose@insightforensics.com" class="btn">Contact Ann</a>
+                                <a href="mailto:ann@insightforensics.com" class="btn">Contact Ann</a>
                             </div>
                         </li>
                         <li>
