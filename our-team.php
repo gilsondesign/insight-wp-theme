c44242f91c83c7799343258353921143cc5f3dae
Adam Gilson <adam@gilsondesign.com>
2014-08-27 20:57:29 -0500

    adding CVs

--- our-team.php
+++ our-team.php
@@ -78,6 +78,7 @@ Template Name: Our Team
                                 <h3 id="reynolds">Curt Reynolds CPA, CFF</h3>
                                 <p>Curt is a 1989 Magna Cum Laude graduate of the University of Wisconsin at Stout. He has been a licensed CPA in Wisconsin since 1993. Since 1989 Curt has focused his career entirely on investigative auditing and the measurement of damages. He has worked on numerous assignments regarding employee dishonesty, business income and litigation cases. Curt has been deposed and has testified in court. He also participates in industry associations and presents seminars to insurance companies and industry groups.</p>
                                 <a href="mailto:curt@insightforensic.com" class="btn">Contact Curt</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/reynolds-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                         <li>
@@ -86,6 +87,7 @@ Template Name: Our Team
                                 <h3 id="bestul">Jennifer Bestul, CPA</h3>
                                 <p>Jennifer is a 1985 graduate of Luther College, Decorah, IA, with a Bachelor of Arts degree, and a Major in Accounting. Since she began her career in 1985, she has specialized in investigative accounting. She has been a licensed CPA since 1987. Jennifer has worked on numerous assignments including business income, property damage, extra expense, worker’s comp, inventory, employee dishonesty, and measurement of lost wages. Jennifer has been deposed, participated in appraisals, testified in court and has lectured before insurance companies on several topics.</p>
                                 <a href="mailto:jennifer@insightforensic.com" class="btn">Contact Jennifer</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/bestul-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                         <li>
@@ -94,6 +96,7 @@ Template Name: Our Team
                                 <h3 id="friestedt">John D. Friestedt, CPA, CFF</h3>
                                 <p>John is a 1981 Cum Laude graduate of the University of Notre Dame. He also received an MBA in 1984 from the University of Texas at Austin. He is a licensed CPA in the states of Wisconsin and Illinois. Since 1986, John has focused his entire career on forensic accounting, including numerous assignments involving lost business income, third party income losses and employee dishonesty losses. John has been deposed a number of times and has testified in court.</p>
                                 <a href="mailto:john@insightforensic.com" class="btn">Contact John</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/friestedt-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                         <li>
@@ -102,6 +105,7 @@ Template Name: Our Team
                                 <h3 id="denson">Kathy M Denson, CPA, CFF, CFE</h3>
                                 <p>Kathy graduated from Northeastern Illinois University (with high honors) in 1983, with a bachelors degree in Business with an emphasis on accounting. She became a licensed CPA in Illinois in 1986 and in Colorado in 1989. Kathy’s experience includes 3 years performing financial statement audits,  but the majority of her career has been devoted to determining lost income of businesses and individuals, and providing expert testimony on financial damages.</p>
                                 <a href="mailto:kathy@insightforensic.com" class="btn">Contact Kathy</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/denson-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                         <li>
@@ -110,6 +114,7 @@ Template Name: Our Team
                                 <h3 id="knight">Terrence S. Knight, CPA, CFF</h3>
                                 <p>Terry graduated in 1983 from Marquette University, Milwaukee, WI, with a Bachelor of Science in Accounting.  Terry is a licensed CPA in Illinois and has focused his career exclusively on forensic accounting since 1991. He has been engaged in numerous assignments relating to employee dishonesty losses, business income losses, property damage, financial condition assessments, and disability income losses. Terry has been deposed and has testified in court and has presented seminars to members of the insurance industry and legal profession.</p>
                                 <a href="mailto:terry@insightforensic.com" class="btn">Contact Terrence</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/knight-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
 
@@ -125,6 +130,7 @@ Template Name: Our Team
                                 <h3 id="conway">Kelly M. Conway, CPA</h3>
                                 <p>Kelly has a BS from Northern Michigan University.  She is a CPA and has been with our firm since 2003.  She had prior work experience as a bookkeeper for a construction firm and with a tax/write-up accounting firm.  At Insight, Kelly specializes in 1st party property, and lost income analysis. </p>
                                 <a href="mailto:kelly@insightforensic.com" class="btn">Contact Kelly</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/conway-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                         <li>
@@ -141,6 +147,7 @@ Template Name: Our Team
                                 <h3 id="schultz">Jessica M. Schultz, CPA</h3>
                                 <p>Jessie has a BS from Marquette University.  She is a CPA and has been with our firm since 1997.  She had prior work experience as an administrator in a title company.  At Insight, Jessica works on damage assessment for a wide array of insurance and litigated matters. </p>
                                 <a href="mailto:jessica@insightforensic.com" class="btn">Contact Jessica</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/schultz-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                         <li>
@@ -149,6 +156,7 @@ Template Name: Our Team
                                 <h3 id="derose">Ann F. DeRose, CPA</h3>
                                 <p>Ann has a BBA from the University of Wisconsin-Eau Claire.  She is a CPA and has been with us since 2000.  She had prior work experience as a bank manager and with a tax/write-up accounting firm.  At Insight she specializes in the evaluation of lost income matters arising from personal injury, insured events, and torts.  Ann has provided expert witness testimony on behalf of clients.</p>
                                 <a href="mailto:ann@insightforensic.com" class="btn">Contact Ann</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/derose-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                         <li>
@@ -157,6 +165,7 @@ Template Name: Our Team
                                 <h3 id="stone">Sandra M. Stone, CFE</h3>
                                 <p>Sandy has a BA (Cum Laude) degree in Business and Economics from the University of Pittsburgh.  She is a CFE and has over 25 years of professional experience in evaluating claims and in accounting.  For over a decade, Sandy has investigated complex commercial crime, fidelity bond and cyber claims as a Senior Claims Examiner and Officer at Chubb & Son.  She has taught accounting courses and presented at various fidelity/crime conferences.  Sandy has successfully participated in mediations, testified in court, and given numerous depositions.  At Insight Forensic Accountants, she specializes in evaluating losses arising from employee dishonesty and works on 3rd party liability, 1st party property and business income losses.</p>
                                 <a href="mailto:sandy@insightforensic.com" class="btn">Contact Sandy</a>
+								<a href="<?php bloginfo('template_url'); ?>/docs/stone-CV.pdf" target="_blank">Download my CV</a>
                             </div>
                         </li>
                     </ul>
